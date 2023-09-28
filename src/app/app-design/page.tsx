e1aa0c7b822cfe9a07e5d71ef6b442c4323ce42e
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-09-28 18:59:02 +0930

    create and style app design page

--- src/app/app-design/page.tsx
+++ src/app/app-design/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DesignBox from "@/app/components/DesignBox";
 import DesignPageContainer from "@/app/components/DesignPageContainer";
+import Card from "@/app/components/Card";
 
 function Page() {
   return (
@@ -12,7 +13,44 @@ function Page() {
           right at their fingertips.
         </p>
       </DesignBox>
-      <DesignPageContainer heading={"AIRFILTER"} imgName={} />
+      <DesignPageContainer
+        heading={"AIRFILTER"}
+        imgSource={"app-design/desktop/image-airfilter.jpg"}
+        text={
+          "Solving the problem of poor indoor air quality by filtering the air"
+        }
+      />
+      <DesignPageContainer
+        heading={"EYECAM"}
+        imgSource={"app-design/desktop/image-eyecam.jpg"}
+        text={
+          "Product that lets you edit your favorite photos and videos at any time"
+        }
+      />
+      <DesignPageContainer
+        heading={"FACEIT"}
+        imgSource={"app-design/desktop/image-faceit.jpg"}
+        text={
+          "Get to meet your favorite internet superstar with the faceit app"
+        }
+      />
+      <DesignPageContainer
+        heading={"TODO"}
+        imgSource={"app-design/desktop/image-todo.jpg"}
+        text={"A todo app that features cloud sync with light and dark mode"}
+      />
+      <DesignPageContainer
+        heading={"LOOPSTUDIOS"}
+        imgSource={"app-design/desktop/image-loopstudios.jpg"}
+        text={"A VR experience app made for Loopstudios"}
+      />
+      <section className="mt-[5rem] mb-[4rem] mt-[7rem] flex flex-col gap-5 lap desktop:grid grid-cols-2 desktop:gap-8  ">
+        <Card imgSource={"/image-web-design.jpg"} heading={"WEB DESIGN"} />
+        <Card
+          imgSource={"/image-graphic-design.jpg"}
+          heading={"GRAPHIC DESIGN"}
+        />
+      </section>
     </div>
   );
 }
