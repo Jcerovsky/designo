b8fd8c6cb4a4f9c327728387f7cfe47c329b1d17
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-09-29 07:24:52 +0930

    add responsive design to add-design page

--- src/app/app-design/page.tsx
+++ src/app/app-design/page.tsx
@@ -6,44 +6,46 @@ import Card from "@/app/components/Card";
 function Page() {
   return (
     <div>
-      <DesignBox>
+      <DesignBox style="sm:mx-5 mb-[5rem]">
         <h1 className="mb-6 text-4xl">App Design</h1>
         <p className="mx-[4.25rem] desktop:ml-0">
           Our mobile designs bring intuitive digital solutions to your customers
           right at their fingertips.
         </p>
       </DesignBox>
-      <DesignPageContainer
-        heading={"AIRFILTER"}
-        imgSource={"app-design/desktop/image-airfilter.jpg"}
-        text={
-          "Solving the problem of poor indoor air quality by filtering the air"
-        }
-      />
-      <DesignPageContainer
-        heading={"EYECAM"}
-        imgSource={"app-design/desktop/image-eyecam.jpg"}
-        text={
-          "Product that lets you edit your favorite photos and videos at any time"
-        }
-      />
-      <DesignPageContainer
-        heading={"FACEIT"}
-        imgSource={"app-design/desktop/image-faceit.jpg"}
-        text={
-          "Get to meet your favorite internet superstar with the faceit app"
-        }
-      />
-      <DesignPageContainer
-        heading={"TODO"}
-        imgSource={"app-design/desktop/image-todo.jpg"}
-        text={"A todo app that features cloud sync with light and dark mode"}
-      />
-      <DesignPageContainer
-        heading={"LOOPSTUDIOS"}
-        imgSource={"app-design/desktop/image-loopstudios.jpg"}
-        text={"A VR experience app made for Loopstudios"}
-      />
+      <div className="max-w-[72.375rem] gap-[1.875rem] mx-5 grid grid-cols-[repeat(auto-fit,minmax(min(21.875rem,100%),1fr))]">
+        <DesignPageContainer
+          heading={"AIRFILTER"}
+          imgSource={"app-design/desktop/image-airfilter.jpg"}
+          text={
+            "Solving the problem of poor indoor air quality by filtering the air"
+          }
+        />
+        <DesignPageContainer
+          heading={"EYECAM"}
+          imgSource={"app-design/desktop/image-eyecam.jpg"}
+          text={
+            "Product that lets you edit your favorite photos and videos at any time"
+          }
+        />
+        <DesignPageContainer
+          heading={"FACEIT"}
+          imgSource={"app-design/desktop/image-faceit.jpg"}
+          text={
+            "Get to meet your favorite internet superstar with the faceit app"
+          }
+        />
+        <DesignPageContainer
+          heading={"TODO"}
+          imgSource={"app-design/desktop/image-todo.jpg"}
+          text={"A todo app that features cloud sync with light and dark mode"}
+        />
+        <DesignPageContainer
+          heading={"LOOPSTUDIOS"}
+          imgSource={"app-design/desktop/image-loopstudios.jpg"}
+          text={"A VR experience app made for Loopstudios"}
+        />
+      </div>
       <section className="mt-[5rem] mb-[4rem] mt-[7rem] flex flex-col gap-5 lap desktop:grid grid-cols-2 desktop:gap-8  ">
         <Card
           imgSource={"/image-web-design.jpg"}
